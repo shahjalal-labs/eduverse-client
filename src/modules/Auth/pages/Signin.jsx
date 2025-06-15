21d9782b7e2c6f5261c4aab584664febba961f99
sj <muhommodshahjalal@gmail.com>
2025-06-15 11:45:10 +0600

    signin page theme toggle

--- src/modules/Auth/pages/Signin.jsx
+++ src/modules/Auth/pages/Signin.jsx
@@ -1,28 +1,16 @@
-import { Link } from "react-router";
 import { motion } from "motion/react";
 import SigninForm from "../components/SigninForm";
 import LottieAnimation from "../../../animation/LottieAnimation";
 import { authLottie } from "../../../animation";
-import {
-  leftToRight,
-  rightToLeft,
-  topToBottom,
-} from "../../../animation/motion";
+import { leftToRight, rightToLeft } from "../../../animation/motion";
 import UseHelmet from "../../../hooks/useHelmet";
+import SigninText from "../components/SigninText";
 
 const Signin = () => {
   return (
     <div className="min-h-[80vh] mt-10 g-gray-100 py-10 rounded-lg shadow hover-effect">
       <UseHelmet title="Sign in" />
-      <motion.div className="max-lg:max-w-[490px] mx-auto" {...topToBottom}>
-        <h2 className="text-center text-xl sm:text-3xl font-semibold font-kaushan text-purple-700">
-          Eduverse connects talent with opportunity.
-        </h2>
-        <p className="text-center text-gray-500 ext-sm mb-10 mt-2">
-          Unlock collaborative learning, connect with your study friends, and
-          ace your assignments together.
-        </p>
-      </motion.div>
+      <SigninText />
       <div className="flex  flex-col-reverse lg:flex-row items-center justify-around *:flex-1 gap-7  mt-5">
         <motion.div
           className="lg:max-w-[480px]  order bg-gray-50 shadow-lg rounded-lg hover:shadow-sm hover:scale-98"
