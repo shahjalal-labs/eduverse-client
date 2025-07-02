72e87bb09429011786eab810f2f89cb2b13f3c14
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-02 09:37:42 +0600

    feat: Added nav/NavbarStart.jsx

---
+++ src/modules/shared/Layout/nav/NavbarStart.jsx
@@ -0,0 +1,36 @@
+import navbarLinks from "../navbarLinksConstant";
+
+const NavbarStart = () => {
+  return (
+    <div className="navbar-start">
+      <div className="dropdown dropdown-right dropdown-hover ">
+        <div tabIndex={0} role="button" className=" lg:hidden cursor-pointer">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-5 w-5"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            {" "}
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M4 6h16M4 12h8m-8 6h16"
+            />{" "}
+          </svg>
+        </div>
+        <ul
+          tabIndex={0}
+          className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1  p-2 shadow mt-6 min-w-[170px]"
+        >
+          {navbarLinks()}
+        </ul>
+      </div>
+      <Brand />
+    </div>
+  );
+};
+
+export default NavbarStart;
