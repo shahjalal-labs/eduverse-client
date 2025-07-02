49b23e6750d5f5e597c466f469e0de2522f5fe28
Md Shahjalal <muhommodshahjalal@gmail.com>
2025-07-02 09:37:56 +0600

    improve: Updated Layout/Navbar.jsx

--- src/modules/shared/Layout/Navbar.jsx
+++ src/modules/shared/Layout/Navbar.jsx
@@ -7,6 +7,7 @@ import Brand from "../ui/Brand";
 import Avatar from "../ui/Avatar";
 import navbarLinks from "./navbarLinksConstant";
 import DropDown from "../ui/DropDown";
+import NavbarStart from "./nav/NavbarStart";
 
 const Navbar = () => {
   const { user } = useAuth();
@@ -16,34 +17,7 @@ const Navbar = () => {
       className="navbar bg-base-100 shadow-sm z-10 sticky top-[1px]"
       {...topToBottom}
     >
-      <div className="navbar-start">
-        <div className="dropdown dropdown-right dropdown-hover ">
-          <div tabIndex={0} role="button" className=" lg:hidden cursor-pointer">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-5 w-5"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              {" "}
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M4 6h16M4 12h8m-8 6h16"
-              />{" "}
-            </svg>
-          </div>
-          <ul
-            tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 min-w-[150px] p-2 shadow mt-6 min-w-[170px]"
-          >
-            {navbarLinks()}
-          </ul>
-        </div>
-        <Brand />
-      </div>
+      <NavbarStart />
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navbarLinks()}</ul>
       </div>
